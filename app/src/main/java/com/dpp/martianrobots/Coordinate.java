526b989fbcbabfe770f6809157c5d35e8474cdfc
Susana Anido <suanido@gmail.com>
2019-03-18 22:41:30 +0100

    Change forgotten in the last commit

--- app/src/main/java/com/dpp/martianrobots/Coordinate.java
+++ app/src/main/java/com/dpp/martianrobots/Coordinate.java
@@ -1,9 +1,25 @@
 package com.dpp.martianrobots;
 
+import java.util.Objects;
+
 public class Coordinate {
 
     private int x;
     private int y;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return x == that.x &&
+                y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 
     public Coordinate(int x, int y) {
         this.x = x;
